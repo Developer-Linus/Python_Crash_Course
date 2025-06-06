6e695be4ca415cd3b28d2ae73e71532c8b156556
Developer-Linus <linusmksu30@gmail.com>
2025-06-06 12:48:31 +0300

    Lists: Learnt about appending, inserting, and deleting elements in a list

---
+++ lists/exercise/guest_list.py
@@ -0,0 +1,4 @@
+guests = ["Nathan", "Ezra", "Memo"]
+print(guests[0] + ', you are invited to my party')
+print(guests[1] + ', you are invited to my party')
+print(guests[2] + ', you are invited to my party')
