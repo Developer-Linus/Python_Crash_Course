213fa96a89308525508887efc22349e5e6ea433f
Developer-Linus <linusmksu30@gmail.com>
2025-07-07 12:02:39 +0300

    Learnt about modifying and deleting key-value pairs in Python dictionary.

---
+++ dictionaries/exercise/person.py
@@ -0,0 +1,12 @@
+person = {
+    'first_name': 'Edward',
+    'last_name': 'Bosuben',
+    'age': 40,
+    'city': 'Bomet',
+}
+
+print('Edward Personal information is:\n')
+print("First name: " + person['first_name'])
+print("Last name: " + person['last_name'])
+print("Age: " + str(person['age']))
+print("City: " + person['city'])
