33397a5ceaedc17c6fcdddbed9567f12f6ccf8aa
Developer-Linus <linusmksu30@gmail.com>
2025-08-05 16:47:01 +0300

    Learnt about passing arbitray, arbitrary keyword, and mixing positional and arbitrary arguments in funcitons

---
+++ functions/exercise/sandwiches.py
@@ -0,0 +1,17 @@
+def make_sandwich(*sandwich_items):
+    print("\nA sandwith with the following items is being made: ")
+    for item in sandwich_items:
+        print(item)
+
+
+make_sandwich("Chicken Mayo",    
+              "Egg Salad",       
+              "Tuna Melt",
+              "Grilled Cheese")
+make_sandwich("Ham & Cheese",
+    "Turkey Club",
+    "Veggie Delight",
+    "Beef BLT", 
+    "Avocado Toast",
+    "Pulled Pork"
+    )
